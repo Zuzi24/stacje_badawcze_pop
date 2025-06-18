f7383b6abe36890818be3879d10b25a9e1da9257
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-18 19:56:50 +0200

    Dodano GUI z Tkinter do zarządzania danymi za pomocą przycisków

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Główna aplikacja GUI
+import tkinter as tk
+
+def gui_main():
+    root = tk.Tk()
+    root.title("System zarządzania siecią badawczą")
+
+    tk.Button(root, text="Stacje badawcze").pack(pady=5)
+    tk.Button(root, text="Pracownicy").pack(pady=5)
+    tk.Button(root, text="Klienci").pack(pady=5)
+
+    tk.Button(root, text="Zamknij", command=root.destroy).pack(pady=20)
+    root.mainloop()
+
+if __name__ == "__main__":
+    gui_main()
