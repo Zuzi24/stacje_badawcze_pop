6b79259d7d2737df51c44ca0352349e36272f61b
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-18 19:01:29 +0200

    Funkcja pobierania współrzędnych miast z Wikipedii

--- utils/controller.py
+++ utils/controller.py
@@ -1,1 +1,15 @@
 # Logika pobierania danych i tworzenia map
+import requests
+from bs4 import BeautifulSoup
+
+def get_coordinates(city_name: str) -> list:
+    url = f"https://pl.wikipedia.org/wiki/{city_name}"
+    try:
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, "html.parser")
+        latitude = float(soup.select(".latitude")[1].text.replace(",", "."))
+        longitude = float(soup.select(".longitude")[1].text.replace(",", "."))
+        return [latitude, longitude]
+    except Exception as e:
+        print("Błąd pobierania współrzędnych:", e)
+        return [52.2, 21.0]
