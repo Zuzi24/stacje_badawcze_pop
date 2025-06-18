4c56bad27bf07333c26e7d37074bfd3e2e1bbec1
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-18 19:28:33 +0200

    Generowanie mapy z lokalizacjami i danymi

--- utils/controller.py
+++ utils/controller.py
@@ -1,6 +1,9 @@
 # Logika pobierania danych i tworzenia map
+from collections import defaultdict
 import requests
 from bs4 import BeautifulSoup
+import folium
+
 
 def get_coordinates(city_name: str) -> list:
     url = f"https://pl.wikipedia.org/wiki/{city_name}"
@@ -12,4 +15,28 @@ def get_coordinates(city_name: str) -> list:
         return [latitude, longitude]
     except Exception as e:
         print("Błąd pobierania współrzędnych:", e)
-        return [52.2, 21.0]
+        return [52.2, 21.0] #domyślna lokalizacja
+
+
+def get_grouped_map(data: list, filename="mapa.html") -> None:
+    grouped = defaultdict(list)
+
+    for item in data:
+        grouped[item['location']].append(item['name'])
+
+    mapa = folium.Map(location=[52.21, 21.0], zoom_start=6)
+
+    for location, names in grouped.items():
+        coord = get_coordinates(location)
+
+        #  WAŻNA LOKALIZACJA – TU DODAJ IMIONA DO POPUPA
+        popup_text = "<br>".join(["• " + name for name in names])  #  lista klientów
+        tooltip_text = ", ".join(names)  # na podgląd
+
+        folium.Marker(
+            location=coord,
+            popup=popup_text,
+            tooltip=tooltip_text
+        ).add_to(mapa)
+
+    mapa.save(filename)
